ae95d8a0dfa6d059d2baa6ae6ec16e76ad310919
Anshu Sarkar <anshusarkaranx@gmail.com>
2022-09-03 00:30:13 +0530

    about and contact route added

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("home", { content: homeStartingContent });
 });
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
